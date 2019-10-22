1789bfdf8aea2d7f155018313ae6a384c61f8761
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-10-22 02:16:48 -0600

    Add cypress tests for navigation and h1.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,4 +1,4 @@
-describe('Sapper template app', () => {
+describe('/', () => {
 	beforeEach(() => {
 		cy.visit('/')
 	});
@@ -7,13 +7,48 @@ describe('Sapper template app', () => {
 		cy.contains('h1', 'Great success!')
 	});
 
-	it('navigates to /about', () => {
-		cy.get('nav a').contains('about').click();
-		cy.url().should('include', '/about');
+	it('navigates to /designers', () => {
+		cy.get('nav a').contains('DESIGNERS').click();
+		cy.url().should('include', '/designers');
+	});
+
+	it('navigates to /clothing', () => {
+		cy.get('nav a').contains('CLOTHING').click();
+		cy.url().should('include', '/clothing');
+	});
+
+	it('navigates to /shoes', () => {
+		cy.get('nav a').contains('SHOES').click();
+		cy.url().should('include', '/shoes');
+	});
+
+	it('navigates to /bags', () => {
+		cy.get('nav a').contains('BAGS').click();
+		cy.url().should('include', '/bags');
+	});
+
+	it('navigates to /accessories', () => {
+		cy.get('nav a').contains('ACCESSORIES').click();
+		cy.url().should('include', '/accessories');
+	});
+
+	it('navigates to /communities', () => {
+		cy.get('nav a').contains('COMMUNITIES').click();
+		cy.url().should('include', '/communities');
 	});
 
 	it('navigates to /blog', () => {
-		cy.get('nav a').contains('blog').click();
+		cy.get('nav a').contains('BLOG').click();
 		cy.url().should('include', '/blog');
+	});
+});
+
+describe('/designers', () => {
+	beforeEach(() => {
+		cy.visit('/designers')
+	});
+
+	it('has the correct <h1>', () => {
+		cy.contains('h1', 'Designers');
 	});
 });
